100306ff21891f2d5094bddae252a26148542c78
Erwan Jugand <erwan.jugand@gmail.com>
2021-10-16 17:44:30 +0200

    init + first test

---
+++ layouts/custom.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <Head>
+      <Title>{{ title }}</Title>
+    </Head>
+    <header>
+      <slot name="header" />
+    </header>
+
+    <main>
+      <slot />
+    </main>
+
+    <footer>
+      <slot name="footer" />
+    </footer>
+  </div>
+</template>
+
+<style lang="css">
+main {
+  background: red;
+}
+</style>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    title: String
+  }
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <NuxtLayout name="custom" :title="title">
+    <template #header>
+      {{ header}}
+    </template>
+    <h1 v-text="title" />
+    <Button>
+      Click Me !
+    </Button>
+  </NuxtLayout>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  layout: false,
+
+  data: () => ({
+    header: 'Header',
+    title: 'Nuxt 3 app example'
+  }),
+
+})
+</script>
+
